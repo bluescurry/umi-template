b3f3dac1ba0dc20dfa9de8e1ad260c5ea880430d
bluescurry <18832643723@163.com>
2020-12-21 22:10:10 +0800

    增加 proxy 配置

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,30 +1,34 @@
+import chalk from 'chalk';
+import { getShellQuery } from './utils';
+
+interface ProxyMap {
+  [propName: string]: string;
+}
+
+/** 代理映射关系 */
+const proxyMap: ProxyMap = {
+  mock: 'http://xxxx',
+  xiaoming: 'http://xxxx',
+};
+
+/**
+ * 切环境改这里
+ * eg: npm run start proxy=rdname
+ */
+const proxyTarget: string = getShellQuery('proxy') ?? proxyMap[0];
+console.log(chalk.bold.blue('连接代理服务端： ') + chalk.bold.green(proxyTarget));
+
 /**
  * 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
- * The agent cannot take effect in the production environment
- * so there is no configuration of the production environment
- * For details, please see
- * https://pro.ant.design/docs/deploy
  */
 export default {
   dev: {
-    '/api/': {
-      target: 'https://preview.pro.ant.design',
+    // 这里维护接口代码配置
+    '/api': {
+      target: proxyMap[proxyTarget],
       changeOrigin: true,
-      pathRewrite: { '^': '' },
     },
   },
-  test: {
-    '/api/': {
-      target: 'https://preview.pro.ant.design',
-      changeOrigin: true,
-      pathRewrite: { '^': '' },
-    },
-  },
-  pre: {
-    '/api/': {
-      target: 'your pre url',
-      changeOrigin: true,
-      pathRewrite: { '^': '' },
-    },
-  },
+  pre: {},
+  mock: {},
 };
---
+++ config/utils.ts
@@ -0,0 +1,22 @@
+/**
+ * 从 npm 启动命令中获取指定参数
+ * 比如从 npm run dev --proxy=mock 得到 proxy 的值为 mock
+ * @param field 要获取的参数名
+ * @returns 返回的参数值，没有则返回 undefined
+ */
+export const getShellQuery = (field: string): string | undefined => {
+  // 启动命令拆分对象
+  const argv = JSON.parse(process.env.npm_config_argv ?? '');
+  if (argv.original) {
+    // eslint-disable-next-line no-bitwise
+    let fieldValue = argv.original.find((item: string) => ~item.indexOf(`--${field}=`));
+
+    if (fieldValue) {
+      fieldValue = fieldValue.replace(`--${field}=`, '');
+    }
+
+    return fieldValue;
+  }
+
+  return undefined;
+};
